f36a1a89bfbe9193c6dfb9271c77b92735eb7542
jinine <tengen@mnbc.ca>
2022-06-20 14:02:54 -0700

    added small box

--- mnbc_museum/src/App.js
+++ mnbc_museum/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Canvas} from 'react-three-fiber';
 
 function App() {
-  return (<div>
+  return (<>
     
-    Hello
+    <Canvas>
+      <mesh>
+        <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
+        <meshStandardMaterial attach='material'/>
+      </mesh>
+    </Canvas>
 
-  </div>);
+  </>);
 }
 
 export default App;
