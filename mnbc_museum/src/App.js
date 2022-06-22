b545ffdd997dfc11f63f830b7c40dc12ce33d686
jinine <tengen@mnbc.ca>
2022-06-22 14:43:26 -0700

    completed 3D shapes

--- mnbc_museum/src/App.js
+++ mnbc_museum/src/App.js
@@ -1,19 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
 import {softShadows, MeshWobbleMaterial, OrbitControls} from '@react-three/drei';
+import {useSpring, animated} from '@react-spring/three';
 
 softShadows();
 
 const SpinningMesh = ( { position, args, color, speed }) =>{ 
   const mesh = useRef(null);
   useFrame(() => (mesh.current. rotation.x = mesh.current.rotation.y += 0.01));
+
+  //
+  const [expand, setExpand] = useState(false);
+
+  const props = useSpring({
+    scale: expand ? [1.6,1.6,1.6] : [1,1,1],
+  });
+
   return(
-    <mesh castShadow position = {position} ref = {mesh}>
+    <animated.mesh 
+      OnClick={() => setExpand(!expand)}
+      scale={props.scale}
+      castShadow position = {position} ref = {mesh}>
         <boxBufferGeometry attach='geometry' args={args}/>
         <MeshWobbleMaterial attach='material' color={color} speed={speed} factor={.6}/>
-      </mesh>
+      </animated.mesh>
   )
 }
 
@@ -45,11 +57,12 @@ function App() {
         position={[0,-3,0]}>
           <planeBufferGeometry attach='geometry' args={[100,100]}/>
           <shadowMaterial attach='material' opacity={0.3}/>
-        </mesh>
+        </mesh> 
+        <SpinningMesh position = { [0, 1, 0] } args={[3,2,1]} color='lightblue' speed={2}/>
+        <SpinningMesh position = { [-2, 1, -5] } color='pink'  speed={6} />
+        <SpinningMesh position = { [5, 1, -2] } color='pink'  speed={6}/>
       </group>
-      <SpinningMesh position = { [0, 1, 0] } args={[3,2,1]} color='lightblue' speed={2}/>
-      <SpinningMesh position = { [-2, 1, -5] } color='pink'  speed={6} />
-      <SpinningMesh position = { [5, 1, -2] } color='pink'  speed={6}/>
+     
       <OrbitControls />
     </Canvas>
 
