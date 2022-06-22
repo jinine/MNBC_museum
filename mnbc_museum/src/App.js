b74fe852c991995137144b65500b8947461e7e74
jinine <tengen@mnbc.ca>
2022-06-22 13:52:26 -0700

    shapes added

--- mnbc_museum/src/App.js
+++ mnbc_museum/src/App.js
@@ -1,16 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import {Canvas} from 'react-three-fiber';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+
+const SpinningMesh = ( { position, args, color }) =>{ 
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current. rotation.x = mesh.current.rotation.y += 0.01));
+  return(
+    <mesh castShadow position = {position} ref = {mesh}>
+        <boxBufferGeometry attach='geometry' args={args}/>
+        <meshStandardMaterial attach='material' color={color} />
+      </mesh>
+  )
+}
 
 function App() {
+ 
   return (<>
     
-    <Canvas>
-      <mesh>
-        <boxBufferGeometry attach='geometry' args={[1,1,1]}/>
-        <meshStandardMaterial attach='material'/>
-      </mesh>
+    <Canvas shadows camera={{position: [-5, 2, 10], fov: 60}}>
+      <ambientLight intensity={.7} />
+      <directionalLight 
+      castShadow 
+      position={[0, 10, 0]} 
+      intensity = {1.5} 
+      shadow-mapSize-width={1024} 
+      shadow-mapSize-height={1024}
+      shadow-camera-far={50} 
+      shadow-camera-left={-10} 
+      shadow-camera-right={10}
+      shadow-camera-top={10} 
+      shadow-camera-bottom={-10}/>
+
+      <pointLight position={ [-10, 0,-20] } intensity={.5}/>
+      <pointLight position={ [0, -10, 0] } intensity={1.5}/>
+
+      <group>
+        <mesh 
+        receiveShadow 
+        rotation={[-Math.PI / 2, 0, 0]} 
+        position={[0,-3,0]}>
+          <planeBufferGeometry attach='geometry' args={[100,100]}/>
+          <shadowMaterial attach='material' opacity={0.3}/>
+        </mesh>
+      </group>
+      <SpinningMesh position = { [0, 1, 0] } args={[3,2,1]} color='lightblue' />
+      <SpinningMesh position = { [-2, 1, -5] } color='pink' />
+      <SpinningMesh position = { [5, 1, -2] } color='pink' />
     </Canvas>
 
   </>);
