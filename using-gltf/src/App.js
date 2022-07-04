bb7c25df4a4db9e20184b943755ee4823d4cdb2a
jinine <tengen@mnbc.ca>
2022-07-04 15:19:13 -0700

    loaded gltf model

--- using-gltf/src/App.js
+++ using-gltf/src/App.js
@@ -1,26 +1,47 @@
-import { Suspense } from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { Html, useProgress, useGLTF } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Suspense, useRef, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import {OrbitControls, useGLTF } from '@react-three/drei';
+import './index.css'
 
-function Loader() {
-  const { progress } = useProgress();
-  return <Html center>{progress} % loaded</Html>;
+
+function Model({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/scene.gltf')
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group position={[0, -0.39, 0.73]} rotation={[-0.18, 0, -0.01]}>
+        <group rotation={[-Math.PI, 0, 0]}>
+          <mesh geometry={nodes.Model_material0_0.geometry} material={materials.material0} />
+          <mesh geometry={nodes.Model_material0_0_1.geometry} material={materials.material0} />
+          <mesh geometry={nodes.Model_material0_0_2.geometry} material={materials.material0} />
+        </group>
+      </group>
+    </group>
+  )
 }
 
-const LoadItem = (args) => {
-  const gltf = useLoader(useGLTF, './assets/artifact/scene.gltf');
-  return <primitive object={gltf.scene} {...args} />;
-};
-
 function App() {
+
   return (
-    <Canvas style={{ height: '100vh' }}>
-      <Suspense fallback={<Loader />}>
-        <pointLight color="yellow" position={[10, 10, 10]} />
-        <LoadItem position={[1, 1, 1]} scale={0.2} rotation-x={1} />
-      </Suspense>
-    </Canvas>
+    <div className = "App">
+          <div className="wrapper">
+        <div className="card"> 
+            <div className="product-canvas">
+              <Canvas>
+                <Suspense fallback={null}>
+                  <ambientLight />
+                  <spotLight intensity={0.9} angle={0.1} penumbra={1} position={[10,15,10]} castShadow/>
+                 <Model scale={1}/>
+                 <OrbitControls enablePan={true} enableRotate = {true} enableZoom = {true} />
+                </Suspense>
+              </Canvas>
+            </div>
+       
+        </div>
+    </div>
+     
+
+    </div>
   );
 }
 
